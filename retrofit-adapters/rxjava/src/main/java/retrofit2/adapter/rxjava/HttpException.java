81f8dcf3163da15a442bbe18eadc0581dd212aa8
morening <sunning_tj@163.com>
2018-08-29 23:22:45 +0800

    Initial commit

---
+++ retrofit-adapters/rxjava/src/main/java/retrofit2/adapter/rxjava/HttpException.java
@@ -0,0 +1,13 @@
+package retrofit2.adapter.rxjava;
+
+import retrofit2.Response;
+
+/**
+ * @deprecated Use {@link retrofit2.HttpException}.
+ */
+@Deprecated
+public final class HttpException extends retrofit2.HttpException {
+    public HttpException(Response<?> response) {
+        super(response);
+    }
+}
